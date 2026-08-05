7a84e14894160cfe01438f61e65e97046667e112
FDS_2023_1 <fds_2023_1@FDS-2023-1noMacBook-Pro.local>
2026-08-05 14:37:06 +0900

    chore: Minor touch to ensure GitHub Pages deployment triggers

--- ascii_dump.py
+++ ascii_dump.py
@@ -1,10 +1,9 @@
 import cv2
+import sys
 import numpy as np
-img = cv2.imread('assets/outer_collar.png')
-# resize to 32x32 for ascii
+img = cv2.imread(sys.argv[1])
 small = cv2.resize(img, (64, 64))
 gray = cv2.cvtColor(small, cv2.COLOR_BGR2GRAY)
-
 chars = " .:-=+*#%@"
 for row in gray:
     line = ""
